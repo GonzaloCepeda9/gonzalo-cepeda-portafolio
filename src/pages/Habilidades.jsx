2fa44c264f41fbf435739d880a0a060a973892f8
Gonzalo Cepeda <gjcepeda9@gmail.com>
2023-05-02 14:19:06 -0300

    Agregado de Habilidades Git y Express. Detalles de estilo.

--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -1,17 +1,19 @@
 import { Box, Divider, Typography } from "@mui/material";
-import HTML from "../imagenes/habilidades/HTML.svg";
 import CSS from "../imagenes/habilidades/CSS.svg";
+import ExpressJS from "../imagenes/habilidades/ExpressJS.svg";
+import Git from "../imagenes/habilidades/Git.svg";
+import HTML from "../imagenes/habilidades/HTML.svg";
 import JS from "../imagenes/habilidades/JS.svg";
-import ReactJS from "../imagenes/habilidades/ReactJS.svg";
-import NodeJS from "../imagenes/habilidades/NodeJS.svg";
+import MaterialUI from "../imagenes/habilidades/MaterialUI.svg";
 import MongoDB from "../imagenes/habilidades/MongoDB.svg";
 import MySQL from "../imagenes/habilidades/MySQL.svg";
-import MaterialUI from "../imagenes/habilidades/MaterialUI.svg";
+import NodeJS from "../imagenes/habilidades/NodeJS.svg";
+import ReactJS from "../imagenes/habilidades/ReactJS.svg";
 import TailwindCSS from "../imagenes/habilidades/TailwindCSS.svg";
 import globalStyles from "../global-styles/globalStyles";
 import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles((theme)=> ({
+const useStyles = makeStyles((theme) => ({
 
   seccionHabilidades: {
     boxShadow: `0px 3px 5px -1px ${theme.palette.tertiary.light}`,
@@ -55,6 +57,7 @@ const useStyles = makeStyles((theme)=> ({
     height: "10rem",
     boxShadow: `0 .2rem .4rem .01rem ${theme.palette.tertiary.light}`,
     borderRadius: "10%",
+    textTransform: "uppercase",
     "&:hover": {
       rotate: "1deg",
       backgroundColor: "#111"
@@ -89,12 +92,77 @@ function Habilidades() {
           fontSize: { xs: "1.5rem", sm: "1.75rem", md: "2rem", lg: "2.25" }
         }}
       >Habilidades</Typography>
+
       <Typography className={`${globalClasses.subtituloSeccion} ${classes.subtituloSeccionHabilidades}`}
         sx={{
           fontFamily: "Acme",
           fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.75rem", lg: "2" }
         }}
-      >Lenguajes / Librerías</Typography>
+      >MERN Stack</Typography>
+
+      <Box className={classes.contenedorHabilidades}>
+        <Box
+          className={classes.contenedorHabilidad}
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" }
+          }}
+          data-aos="flip-right"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+        >
+          <img className={classes.iconHabilidad} src={MongoDB} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>Mongo DB</Typography>
+        </Box>
+
+        <Box
+          data-aos="flip-right"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" },
+          }}
+          className={classes.contenedorHabilidad}>
+          <img style={{ filter: "invert(100%)" }} className={classes.iconHabilidad} src={ExpressJS} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>Express JS</Typography>
+        </Box>
+
+        <Box
+          data-aos="flip-right"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" }
+          }}
+          className={classes.contenedorHabilidad}>
+          <img className={classes.iconHabilidad} src={ReactJS} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>React JS</Typography>
+        </Box>
+
+        <Box
+          data-aos="flip-right"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" }
+          }}
+          className={classes.contenedorHabilidad}>
+          <img className={classes.iconHabilidad} src={NodeJS} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>Node JS</Typography>
+        </Box>
+      </Box>
+
+      <Divider className={globalClasses.divider} sx={{ marginTop: "2rem", marginBottom: "1rem" }} />
+
+      <Typography className={`${globalClasses.subtituloSeccion} ${classes.subtituloSeccionHabilidades}`}
+        sx={{
+          fontFamily: "Acme",
+          fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.75rem", lg: "2" }
+        }}
+      >Otras</Typography>
       <Box className={classes.contenedorHabilidades}>
         <Box
           data-aos="flip-left"
@@ -106,43 +174,7 @@ function Habilidades() {
           }}
           className={classes.contenedorHabilidad}>
           <img className={classes.iconHabilidad} src={HTML} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>HTML</Typography>
-        </Box>
-        <Box
-          data-aos="flip-up"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="1000"
-          sx={{
-            width: { xs: "6rem", sm: "8rem" },
-            height: { xs: "8rem", sm: "10rem" }
-          }}
-          className={classes.contenedorHabilidad}>
-          <img className={classes.iconHabilidad} src={CSS} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>CSS</Typography>
-        </Box>
-        <Box
-          data-aos="flip-up"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="1000"
-          sx={{
-            width: { xs: "6rem", sm: "8rem" },
-            height: { xs: "8rem", sm: "10rem" }
-          }}
-          className={classes.contenedorHabilidad}>
-          <img className={classes.iconHabilidad} src={JS} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>JavaScript</Typography>
-        </Box>
-        <Box
-          data-aos="flip-right"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="1000"
-          sx={{
-            width: { xs: "6rem", sm: "8rem" },
-            height: { xs: "8rem", sm: "10rem" }
-          }}
-          className={classes.contenedorHabilidad}>
-          <img className={classes.iconHabilidad} src={ReactJS} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>React JS</Typography>
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>HTML</Typography>
         </Box>
         <Box
           data-aos="flip-left"
@@ -153,11 +185,39 @@ function Habilidades() {
             height: { xs: "8rem", sm: "10rem" }
           }}
           className={classes.contenedorHabilidad}>
-          <img className={classes.iconHabilidad} src={NodeJS} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>Node JS</Typography>
+          <img className={classes.iconHabilidad} src={CSS} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>CSS</Typography>
         </Box>
         <Box
-          data-aos="flip-down"
+          data-aos="flip-left"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" }
+          }}
+          className={classes.contenedorHabilidad}>
+          <img className={classes.iconHabilidad} src={JS} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>JavaScript</Typography>
+        </Box>
+        <Box
+          className={classes.contenedorHabilidad}
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" }
+          }}
+          data-aos="flip-left"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+        >
+          <img className={classes.iconHabilidad} src={MySQL} alt="" />
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>MySQL</Typography>
+        </Box>
+      </Box>
+
+      <Box className={classes.contenedorHabilidades}>
+        <Box
+          data-aos="flip-left"
           data-aos-easing="ease-out-cubic"
           data-aos-duration="1000"
           sx={{
@@ -166,10 +226,10 @@ function Habilidades() {
           }}
           className={classes.contenedorHabilidad}>
           <img className={classes.iconHabilidad} src={MaterialUI} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>Material UI</Typography>
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>Material UI</Typography>
         </Box>
         <Box
-          data-aos="flip-right"
+          data-aos="flip-left"
           data-aos-easing="ease-out-cubic"
           data-aos-duration="1000"
           sx={{
@@ -178,7 +238,7 @@ function Habilidades() {
           }}
           className={classes.contenedorHabilidad}>
           <img className={classes.iconHabilidad} src={TailwindCSS} alt="" />
-          <Typography sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }} className={classes.tituloHabilidad}>Tailwind CSS</Typography>
+          <Typography className={classes.tituloHabilidad} sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}>Tailwind CSS</Typography>
         </Box>
       </Box>
 
@@ -189,45 +249,25 @@ function Habilidades() {
           fontFamily: "Acme",
           fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.75rem", lg: "2" }
         }}
-      >Bases de datos</Typography>
-      <Box className={classes.contenedorGeneral}>
-        <Box className={classes.contenedorDataBases}>
-          <Box
-            className={classes.contenedorHabilidad}
-            sx={{
-              width: { xs: "6rem", sm: "8rem" },
-              height: { xs: "8rem", sm: "10rem" }
-            }}
-            data-aos="flip-right"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="1000"
+      >Control de versiones</Typography>
+      <Box className={classes.contenedorHabilidades}>
+        <Box
+          className={classes.contenedorHabilidad}
+          sx={{
+            width: { xs: "6rem", sm: "8rem" },
+            height: { xs: "8rem", sm: "10rem" }
+          }}
+          data-aos="flip-right"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1000"
+        >
+          <img className={classes.iconHabilidad} src={Git} alt="" />
+          <Typography
+            sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}
+            className={classes.tituloHabilidad}
           >
-            <img className={classes.iconHabilidad} src={MongoDB} alt="" />
-            <Typography
-              sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}
-              className={classes.tituloHabilidad}
-            >
-              Mongo DB
-            </Typography>
-          </Box>
-          <Box
-            className={classes.contenedorHabilidad}
-            sx={{
-              width: { xs: "6rem", sm: "8rem" },
-              height: { xs: "8rem", sm: "10rem" }
-            }}
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="1000"
-          >
-            <img className={classes.iconHabilidad} src={MySQL} alt="" />
-            <Typography
-              sx={{ fontSize: { xs: ".75rem" }, fontWeight: "bold" }}
-              className={classes.tituloHabilidad}
-            >
-              MySQL
-            </Typography>
-          </Box>
+            Git
+          </Typography>
         </Box>
       </Box>
     </Box>
